d2b77f9298eff327544f54a8dd3c75e3c6718e2e
unknown <shivaneshsekar@gmail.com>
2022-02-28 00:07:23 +0530

    FIFO along with FIFO_tb working
    Both the Codes are working fine

--- FIFO/FIFO.v
+++ FIFO/FIFO.v
@@ -1,44 +1,51 @@
 // FIFO Design for Router 1x3 
 // lft state -> Shows the diff between header and the packet
+// Made by: G.Shivanesh
 
 module FIFO (
     input resetn,clk,write_enb,soft_reset,read_enb,lfd_state,input [7:0]data_in,output empty,full, output reg [7:0]data_out
 );
 
 reg [8:0]mem[0:15];
-integer i;
+integer i=0;
 reg [3:0]w_point,r_point;   // Points the read ad write address
-integer counter; // read counter 
+reg [5:0]counter=0; // read counter 
 always @(posedge clk) begin
     if(~resetn || soft_reset) begin // Reset code 
-        for (i = 0;i<16;i++ ) begin
+        for (i = 0;i<16;i=i+1 ) begin
             mem[i] <= 9'b0;
         end
         w_point <= 4'b0;       // Read and write pointer to Zero 
         r_point <= 4'b0;
+        counter <= 0;
+        data_out <= 8'b0;
     end
     else begin
         if(write_enb && ~full) begin    // if write enable and if not full are high then we:
             mem[w_point] <= {lfd_state,data_in};    // Write at the particular address 
             w_point <= w_point + 1'b1;  // Write pointer is increased
-            if(mem[w_point] == 1'b1) counter <= mem[w_point][7:2] + 6'b1;
         end
-        else if(read_enb && ~empty && (counter != 6'b0))
+        if(read_enb && ~empty)
         begin
-           data_out <= mem[r_point];
+            if(mem[r_point][8] == 1'b1)
+            begin 
+                counter <= mem[r_point][7:2] + 6'b1;
+                r_point <= r_point + 1;
+                data_out <= mem[r_point][7:0];
+            end
+        else if(counter != 0)
+        begin
+           data_out <= mem[r_point][7:0];
            r_point <= r_point + 4'b1;
            counter <= counter - 1; // Counter should decrement till it reaches 0
         end
-        else if(counter == 6'b0) begin
-            counter <= 6'b0;
-            r_point <= 4'b0;
-            w_poiter <= 4'b0;   
         end
+
     end
 
 end
 
-assign empty = (r_point == 4'b0)?1'b1:1'b0;
+assign empty = (w_point == 4'b0)?1'b1:1'b0;
 assign full = (w_point == 4'b1111)?1'b1:1'b0;
 
 
---
+++ FIFO/FIFO_tb.v
@@ -0,0 +1,100 @@
+// Testbench for FIFO module 
+// Made by: G.Shivanesh
+module FIFO_tb (
+);
+    // Ports are declared as in Main module 
+    reg resetn,clk,write_enb,soft_reset,read_enb,lfd_state;
+    reg [7:0]data_in;
+    wire empty,full;
+    wire [7:0]data_out;
+    parameter T = 5; // Used to generate clock signal
+    parameter D = 2*T; // Used to generate delay
+    integer i=0; //for loop purposes
+    reg [5:0]num; // Determines the depth of packet
+    reg [1:0]add; // Address
+    
+
+    FIFO DUT(resetn,clk,write_enb,soft_reset,read_enb,lfd_state,data_in,empty,full,data_out);
+
+    // Generates Clock Signals 
+    initial begin
+        clk = 0;
+        forever begin
+            #(T) clk = ~clk;
+        end
+    end
+    initial begin
+        initialize; // Initializes the values
+        reset;      // Resets the value
+        delay;      // Delays the simulation
+        write;      // Starts the write process
+        for ( i=1;i<num;i=i+1 ) begin
+            lfd_state = 1'b0;   // For packets = 0
+            data_in = {$random}%256;
+            delay;
+        end
+        delay;
+        stop_write;
+        delay;
+        read;
+        for (i = 0;i<num ;i=i+1 ) begin
+            delay;
+        end
+        delay;
+        $finish;
+    end
+
+
+    // task to write the data
+    task write;
+    begin
+        @(negedge clk) write_enb = 1;
+        lfd_state = 1'b1; // For header MSB = 1
+        num = {$random()}%15;  // Generates Random no for total packet size
+        add = {$random()}%3;
+        data_in = {num,add}; // Last 2 for add next 6 for the packet size 6
+        delay;
+
+    end
+    endtask
+
+    // Stop write
+    task stop_write;
+    begin
+        @(negedge clk) write_enb = 1'b0;
+    end
+    endtask
+
+    task read;
+    begin
+        @(negedge clk) read_enb = 1'b1;
+    end
+    endtask
+
+    // task reset
+    task reset;
+    begin
+        @(negedge clk) resetn = 1'b0;
+        @(negedge clk) resetn = 1'b1;
+    end
+    endtask
+
+    // task used to initialize the inputs
+    task initialize; 
+    begin
+        resetn = 1;
+        write_enb = 0;
+        soft_reset = 0;
+        read_enb = 0;
+        lfd_state = 0;
+        data_in = 8'b0;
+    end
+    endtask
+
+    // Delay
+    task delay;
+    begin
+        #(D);
+    end
+    endtask
+endmodule
